a6ee480c7f425fc11402675e972561da6305773c
Tarcizio Junior <tarcizio.junior@gmail.com>
2024-11-06 19:11:18 -0300

    Alteracao de mensagem de erro no back end

--- b3.investment.calculator.Server/Messages.cs
+++ b3.investment.calculator.Server/Messages.cs
@@ -3,7 +3,7 @@
     public static class Messages
     {
         public const string MSG_01 = "O valor monetário deve ser maior que zero.";
-        public const string MSG_02 = "O número de meses deve ser maior que zero.";        
+        public const string MSG_02 = "O número de meses deve ser maior que 1.";        
         public const string MSG_03 = "O valor monetário deve ser menor que R$ 1.000.000.";
         public const string MSG_04 = "O número de meses deve ser menor que 300.";        
     }
